dc6f6dcab625cd35238c262716e3a8b23b322b1b
Julia Medvedeva <yu.medvedeva@qlean.ru>
2021-11-10 13:52:47 +0300

    add all

--- src/App.js
+++ src/App.js
@@ -1,25 +1,133 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import ReactDOM from "react-dom";
+import {Container} from 'react-bootstrap';
 import './App.css';
+import styled from 'styled-components'
+import BootstrapTest from "./BootstrapTest";
+
+class Form extends Component {
+  constructor(props) {
+    super(props);
+    this.myRef = React.createRef();
+  }
+  
+  componentDidMount() {
+    this.myRef.current.focus()
+  }
+  
+  render() {
+    return (
+      <Container>
+        <form className="w-50 border mt-5 p-3 m-auto"
+              style={{'overflow': 'hidden', 'position': 'relative'}}>
+          <div className="mb-3">
+            <label htmlFor="exampleFormControlInput1" className="form-label">Email address</label>
+            <input ref={this.myRef} type="email" className="form-control" id="exampleFormControlInput1"
+                   placeholder="name@example.com"/>
+          </div>
+          <div className="mb-3">
+            <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
+            <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+          </div>
+        <Portal>
+          <Message></Message>
+        </Portal>
+        </form>
+      </Container>
+    )
+  }
+}
+
+const Portal = (props) => {
+  const node = document.createElement('div');
+  document.body.appendChild(node);
+  
+  
+  return ReactDOM.createPortal(props.children, node);
+}
+
+const Message = () => {
+  return (
+    <div
+      style={{
+        'width': '500px',
+        'height': '150px',
+        'backgroundColor': 'red',
+        'position': 'absolute',
+        'right': '0',
+        'bottom': '0'
+      }}>HI
+    </div>
+  )
+}
+
+const Wrapper = styled.div`
+  width: auto;
+  margin: 80px auto 0 auto;
+  //background-color: lightgray;
+`
+
+// class Gratings extends Component {
+//
+//   render() {
+//     return (
+//       <div className={'mb-3 p-3 border border-' + this.props.color}>
+//         {React.Children.map(this.props.children, child => {
+//           return React.cloneElement(child, {className: 'shadow p-3 m-3 border rounder'})
+//         })}
+//       </div>
+//     )
+//   }
+// }
+// const Message = (props) => {
+//   return(<h2>The counter is {props.counter}</h2>)
+// }
+// class Counter extends Component {
+//   state = {counter: 0}
+//
+//   changeCounter = () => {
+//     this.setState(({counter}) => ({
+//       counter: counter + 1
+//     }))
+//   }
+//
+//   render() {
+//     return (
+//       <>
+//         <button
+//           className={'btn btn-primary'}
+//           onClick={this.changeCounter}>
+//           Touch me
+//         </button>
+//         {this.props.render(this.state.counter)}
+//       </>
+//     )
+//   }
+// }
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <Wrapper>
+      {/*<Counter render={counter => (*/}
+      {/*  <Message counter={counter}></Message>*/}
+      {/*)}/>*/}
+      {/*<BootstrapTest*/}
+      {/*  left={<Gratings color={'alert'}>*/}
+      {/*    <h1>World!</h1>*/}
+      {/*    <h1>World!</h1>*/}
+      {/*    <h1>World!</h1>*/}
+      {/*    <h1>World!</h1>*/}
+      {/*    <h1>World!</h1>*/}
+      {/*    <h1>World!</h1>*/}
+      {/*  </Gratings>}*/}
+      {/*  right={<Gratings color={'primary'}>*/}
+      {/*    <h1>Hello!</h1>*/}
+      {/*  </Gratings>}/>*/}
+      <Form/>
+    </Wrapper>
   );
 }
 
 export default App;
+
---
+++ src/BootstrapTest.js
@@ -0,0 +1,18 @@
+import {Col, Container, Row} from "react-bootstrap";
+
+const BootstrapTest = (props) => {
+  return (
+    <Container className={'mt-5 mb-5'}>
+      <Row>
+        <Col>
+          {props.left}
+        </Col>
+        <Col>
+          {props.right}
+        </Col>
+      </Row>
+    </Container>
+  )
+};
+
+export default BootstrapTest;
